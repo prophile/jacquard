3cc77e6431a45a91c7e30a624bf6559503a3f138
Alistair Lynn <arplynn@gmail.com>
2017-01-12 18:55:35 +0000

    Implement constraints

--- jacquard/users/settings.py
+++ jacquard/users/settings.py
@@ -1,4 +1,5 @@
 import hashlib
+import datetime
 
 
 def get_settings(user_id, storage, directory=None):
@@ -15,6 +16,48 @@ def get_settings(user_id, storage, directory=None):
     experiment_settings = {}
 
     for experiment_def in experiment_definitions:
+        constraints = experiment_def.get('constraints', {})
+
+        if constraints:
+            if directory is None:
+                raise ValueError(
+                    "Cannot evaluate constraints on experiment %r "
+                    "with no directory" % experiment_def['id'],
+                )
+
+            user_entry = directory.lookup(user_id)
+
+            if user_entry is None:
+                if not constraints.get('anonymous', True):
+                    continue
+            else:
+                # Disabled until we can figure out timezone awareness
+                #if (
+                #    constraints.get('joined_before', datetime.datetime.max) >
+                #    user_entry.join_date
+                #):
+                #    continue
+
+                #if (
+                #    constraints.get('joined_after', datetime.datetime.min) >
+                #    user_entry.join_date
+                #):
+                #    continue
+
+                required_tags = constraints.get('required_tags', ())
+
+                if (
+                    required_tags and
+                    any(x not in user_entry.tags for x in required_tags)
+                ):
+                    continue
+
+                excluded_tags = constraints.get('excluded_tags', ())
+
+                if any(x in excluded_tags for x in user_entry.tags):
+                    continue
+
+
         branch = experiment_def['branches'][branch_hash(
             experiment_def['id'],
             user_id,
