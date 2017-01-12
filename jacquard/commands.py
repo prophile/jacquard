129d85b2d72c43e72b3407709239f211aad95072
Alistair Lynn <arplynn@gmail.com>
2017-01-12 14:51:21 +0000

    Add base command type

---
+++ jacquard/commands.py
@@ -0,0 +1,9 @@
+import abc
+
+class BaseCommand(metaclass=abc.ABCMeta):
+    def add_arguments(self, parser):
+        pass
+
+    @abc.abstractmethod
+    def handle(self, config, options):
+        pass
