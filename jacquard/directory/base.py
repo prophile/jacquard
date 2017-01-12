63cfb29420e582c908aa4799f3ca9a49b558af0d
Alistair Lynn <arplynn@gmail.com>
2017-01-12 17:30:10 +0000

    Add Jacquard base directory

---
+++ jacquard/directory/base.py
@@ -0,0 +1,19 @@
+import abc
+import namedtuple
+
+
+UserEntry = namedtuple('UserEntry', 'id join_date tags')
+
+
+class Directory(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def __init__(self, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def lookup(self, user_id):
+        pass
+
+    @abc.abstractmethod
+    def all_users(self):
+        pass
