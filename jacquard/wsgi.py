efd2f20a63d3ca4a578e126ced50c4a32dfb49a5
Alistair Lynn <arplynn@gmail.com>
2017-01-13 00:44:47 +0000

    Add docstring for `jacquard.wsgi`

--- jacquard/wsgi.py
+++ jacquard/wsgi.py
@@ -1,3 +1,15 @@
+"""
+WSGI application target.
+
+This module is designed for use when running the Jacquard server from a WSGI
+web server such as waitress or gunicorn. `jacquard.wsgi` would be the module
+to target, picking up the WSGI application from `app`.
+
+In this case, the configuration file can be specified through the environment
+variable `JACQUARD_CONFIG`; if left unspecified, the file 'config.cfg' in the
+current working directory is assumed.
+"""
+
 import os
 
 from jacquard.service import get_wsgi_app
