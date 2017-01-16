376858411bb3e8abc490da7f2312d1fc119bf600
Alistair Lynn <arplynn@gmail.com>
2017-01-16 14:13:48 +0000

    Inline `meets_constraints`

--- jacquard/experiments/experiment.py
+++ jacquard/experiments/experiment.py
@@ -3,7 +3,7 @@
 import contextlib
 import dateutil.parser
 
-from .constraints import meets_constraints
+from .constraints import FAR_FUTURE, DISTANT_PAST
 
 
 class Experiment(object):
@@ -126,4 +126,61 @@ class Experiment(object):
         raise LookupError("No such branch: %r" % branch_id)
 
     def includes_user(self, user_entry):
-        return meets_constraints(self.constraints, user_entry)
+        """
+        Check whether a user meets the experiment's constraints dict.
+
+        A (hopefully constant time) predicate.
+
+        The keys which are currently supported are:
+
+        anonymous
+          A boolean, representing whether anonymous users (users for whom we have no
+          information from the directory) are permitted.
+
+        joined_before
+          A (string) date: only users who joined before this date are permitted.
+
+        joined_after
+          A (string) date: only users who joined after this date are permitted.
+
+        required_tags
+          A list: if specified, only users with all the given tags are permitted.
+
+        excluded_tags
+          A list: if specified, only users without any of the given tags are permitted.
+
+        All these constraints are optional.
+
+        NB: If `anonymous` is True, which is the default, all anonymous users are
+        permitted, *regardless of other constraints*.
+        """
+        if user_entry is None:
+            return self.constraints.get('anonymous', True)
+
+        if (
+                    user_entry.join_date >
+                    self.constraints.get('joined_before', FAR_FUTURE)
+        ):
+            return False
+
+        if (
+                    user_entry.join_date <
+                    self.constraints.get('joined_after', DISTANT_PAST)
+        ):
+            return False
+
+        required_tags = self.constraints.get('required_tags', ())
+
+        if (
+                    required_tags and
+                    any(x not in user_entry.tags for x in required_tags)
+        ):
+            return False
+
+        excluded_tags = self.constraints.get('excluded_tags', ())
+
+        if any(x in excluded_tags for x in user_entry.tags):
+            return False
+
+        return True
+
