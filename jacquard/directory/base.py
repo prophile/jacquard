6666210c89928902a88c86c077630e3d3c755e82
Alistair Lynn <arplynn@gmail.com>
2017-01-16 15:01:07 +0000

    Add docstrings to `UserEntry`

--- jacquard/directory/base.py
+++ jacquard/directory/base.py
@@ -6,6 +6,18 @@ import collections
 
 UserEntry = collections.namedtuple('UserEntry', 'id join_date tags')
 
+UserEntry.__doc__ = """
+Description of attributes of a single user.
+
+Internally this is a `collections.namedtuple`.
+"""
+
+UserEntry.id.__doc__ = """String user ID."""
+UserEntry.join_date.__doc__ = \
+    """Date at which the user is considered to have joined."""
+UserEntry.tags.__doc__ = \
+    """Container of tags which apply to this user, defined by the directory."""
+
 
 class Directory(metaclass=abc.ABCMeta):
     """User directory."""
