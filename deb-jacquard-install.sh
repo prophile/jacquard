5ab00b180d9f7d88f8c6e9c425932d2090d7b29d
Alistair Lynn <arplynn@gmail.com>
2017-01-17 11:26:50 +0000

    Force /var/jacquard to www-data in the Debian build

--- deb-jacquard-install.sh
+++ deb-jacquard-install.sh
@@ -1,3 +1,5 @@
 touch /var/jacquard/jacquard.db
 chown www-data:www-data /var/jacquard/jacquard.db
+chown www-data:www-data /var/jacquard
 chmod a+rw /var/jacquard/jacquard.db
+chmod a+rwx /var/jacquard
