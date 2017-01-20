724a024efce5c7986afb56607d57eb70060fe113
Alistair Lynn <arplynn@gmail.com>
2017-01-20 08:27:56 +0000

    Avoid accidental unbounded recursion (!)

--- jacquard/service/base.py
+++ jacquard/service/base.py
@@ -55,7 +55,7 @@ class Endpoint(metaclass=abc.ABCMeta):
 
     def reverse(self, name, **kwargs):
         try:
-            reverse = self.reverse
+            reverse = self._reverse
         except AttributeError:
             raise AttributeError(
                 "Unbound endpoint: `reverse` is only available on bound "
