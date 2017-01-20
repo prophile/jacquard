d412fe2b20d59cf42197df75f6dc64972676dda9
Alistair Lynn <arplynn@gmail.com>
2017-01-20 08:02:24 +0000

    Re-export Experiment from the top-level package

--- jacquard/experiments/__init__.py
+++ jacquard/experiments/__init__.py
@@ -4,3 +4,9 @@ Experiment definitions subsystem.
 Currently this part of the code is rather ill-defined in its scope with much
 of the rest of the codebase duplicating code which should belong here.
 """
+
+from .experiment import Experiment
+
+__all__ = (
+    'Experiment',
+)
