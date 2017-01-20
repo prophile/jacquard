9baae7a5c633399fe25ca6961e992b50adcd72b4
Alistair Lynn <arplynn@gmail.com>
2017-01-20 08:16:24 +0000

    Use a level of indirection for helpful error messages

--- jacquard/service/base.py
+++ jacquard/service/base.py
@@ -28,7 +28,37 @@ class Endpoint(metaclass=abc.ABCMeta):
 
     def bind(self, config, request, reverse):
         instance = copy.copy(self)
-        instance.config = config
-        instance.request = request
-        instance.reverse = reverse
+        instance._config = config
+        instance._request = request
+        instance._reverse = reverse
         return instance
+
+    @property
+    def config(self):
+        try:
+            return self._config
+        except AttributeError:
+            raise AttributeError(
+                "Unbound endpoint: `config` is only available on bound "
+                "endpoints",
+            )
+
+    @property
+    def request(self):
+        try:
+            return self._request
+        except AttributeError:
+            raise AttributeError(
+                "Unbound endpoint: `request` is only available on bound "
+                "endpoints",
+            )
+
+    def reverse(self, name, **kwargs):
+        try:
+            reverse = self.reverse
+        except AttributeError:
+            raise AttributeError(
+                "Unbound endpoint: `reverse` is only available on bound "
+                "endpoints",
+            )
+        return reverse(name, **kwargs)
