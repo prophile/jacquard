a5621cba0392243008c7559a820b5ed5ecb2fb0f
Alistair Lynn <arplynn@gmail.com>
2017-02-02 15:16:52 +0000

    Move a bunch of the storage tests to a common 'gauntlet'

--- jacquard/storage/tests/test_cloned_redis.py
+++ jacquard/storage/tests/test_cloned_redis.py
@@ -1,8 +1,8 @@
-import functools
+import logging
+import unittest
 import unittest.mock
 
 import pytest
-import hypothesis
 import hypothesis.strategies
 
 from jacquard.storage.exceptions import Retry
@@ -10,7 +10,11 @@ from jacquard.storage.cloned_redis import (
     ClonedRedisStore,
     resync_all_connections
 )
-from jacquard.storage.testing_utils import arbitrary_key, arbitrary_json
+from jacquard.storage.testing_utils import (
+    StorageGauntlet,
+    arbitrary_key,
+    arbitrary_json
+)
 
 try:
     import fakeredis
@@ -18,142 +22,47 @@ except ImportError:
     fakeredis = None
 
 
-def cloned_redis_test(**kwargs):
-    def decorator(fn):
-        @functools.wraps(fn)
-        @unittest.mock.patch('redis.StrictRedis', fakeredis.FakeStrictRedis)
-        def wrapper(*args, **kwargs):
-            try:
-                fn(*args, **kwargs)
-            finally:
-                fakeredis.FakeStrictRedis().flushall()
-                resync_all_connections()
+def cloned_redis_storage_engine():
+    fakeredis.FakeStrictRedis().flushall()
+    resync_all_connections()
 
-        if kwargs:
-            wrapper = hypothesis.given(**kwargs)(wrapper)
-
-        wrapper = pytest.mark.skipif(
-            fakeredis is None,
-            reason="fakeredis is not installed",
-        )(wrapper)
-
-        return wrapper
-    return decorator
+    with unittest.mock.patch(
+        'redis.StrictRedis',
+        fakeredis.FakeStrictRedis,
+    ):
+        return ClonedRedisStore('<fake redis connection>')
 
 
-@cloned_redis_test()
-def test_smoke():
-    # Check no exceptions appear
-    storage = ClonedRedisStore('')
-
-    with storage.transaction() as store:
-        pass
-
-
-@cloned_redis_test(
-    key=arbitrary_key,
+@pytest.mark.skipif(fakeredis is None, reason="fakeredis is not installed")
+@unittest.mock.patch(
+    'redis.StrictRedis',
+    fakeredis.FakeStrictRedis,
 )
-def test_get_nonexistent_key(key):
-    # Just test this works without errors
-    storage = ClonedRedisStore('')
-
-    with storage.transaction() as store:
-        assert store.get(key) is None
+class ClonedRedisGauntletTest(StorageGauntlet, unittest.TestCase):
+    def open_storage(self):
+        logging.basicConfig(level=logging.DEBUG)
+        return cloned_redis_storage_engine()
 
 
-@cloned_redis_test(
-    key=arbitrary_key,
-    value=arbitrary_json,
-)
-def test_simple_write(key, value):
-    storage = ClonedRedisStore('')
-    with storage.transaction() as store:
-        store[key] = value
-    with storage.transaction() as store:
-        assert store[key] == value
-
-@cloned_redis_test(
-    values=hypothesis.strategies.dictionaries(
-        arbitrary_key,
-        arbitrary_json,
-    ),
-)
-def test_enumerate_keys(values):
-    storage = ClonedRedisStore('')
-
-    with storage.transaction() as store:
-        store.update(values)
-
-    with storage.transaction(read_only=True) as store:
-        assert set(store.keys()) == set(values.keys())
-
-
-@cloned_redis_test(
-    key=arbitrary_key,
-    value1=arbitrary_json,
-    value2=arbitrary_json,
-)
-def test_update_key(key, value1, value2):
-    storage = ClonedRedisStore('')
-
-    with storage.transaction() as store:
-        store[key] = value1
-
-    with storage.transaction() as store:
-        store[key] = value2
-
-    with storage.transaction() as store:
-        assert store[key] == value2
-
-
-@cloned_redis_test(
-    key=arbitrary_key,
-    value=arbitrary_json,
-)
-def test_delete_key(key, value):
-    storage = ClonedRedisStore('')
-
-    with storage.transaction() as store:
-        store[key] = value
-
-    with storage.transaction() as store:
-        del store[key]
-
-    with storage.transaction() as store:
-        assert key not in store
-
-
-@cloned_redis_test(
-    key=arbitrary_key,
-    value=arbitrary_json,
-)
-def test_exceptions_back_out_writes(key, value):
-    storage = ClonedRedisStore('')
-
-    try:
-        with storage.transaction() as store:
-            store[key] = value
-            raise RuntimeError()
-    except RuntimeError:
-        pass
-
-    with storage.transaction() as store:
-        assert key not in store
-
-
-@cloned_redis_test(
-    key=arbitrary_key,
-    value1=arbitrary_json,
-    value2=arbitrary_json,
-    replacement_state=hypothesis.strategies.binary(),
+@pytest.mark.skipif(fakeredis is None, reason="fakeredis is not installed")
+#@hypothesis.given(
+#    key=arbitrary_key,
+#    value1=arbitrary_json,
+#    value2=arbitrary_json,
+#    replacement_state=hypothesis.strategies.uuids(),
+#)
+@unittest.mock.patch(
+    'redis.StrictRedis',
+    fakeredis.FakeStrictRedis,
 )
 def test_raises_retry_on_concurrent_write(
-    key,
-    value1,
-    value2,
-    replacement_state,
+    key='',
+    value1='1',
+    value2='2',
+    replacement_state='aaaa',
 ):
-    storage = ClonedRedisStore('')
+    storage = cloned_redis_storage_engine()
+    logging.basicConfig(level=logging.DEBUG)
 
     with storage.transaction() as store:
         store[key] = value1
@@ -162,6 +71,6 @@ def test_raises_retry_on_concurrent_write(
         with storage.transaction() as store:
             fakeredis.FakeStrictRedis().set(
                 b'jacquard-store:state-key',
-                replacement_state,
+                str(replacement_state).encode('ascii'),
             )
             store[key] = value2
