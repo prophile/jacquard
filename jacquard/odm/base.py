b6ffb0c3c21a04a2d7403067051a158edeb4b744
Alistair Lynn <arplynn@gmail.com>
2017-03-08 10:24:38 +0000

    Field naming

--- jacquard/odm/base.py
+++ jacquard/odm/base.py
@@ -1,7 +1,26 @@
+import sys
+
 from . import inflection
 
 
 class ModelMeta(type):
+    def __init__(self, name, bases, namespace):
+        super().__init__(name, bases, namespace)
+        self._dub_fields()
+
+    def _dub_fields(self):
+        if sys.version_info >= (3, 6):
+            return
+
+        # Emulate __set_name__
+        for field_name, field in vars(self).items():
+            try:
+                set_name = field.__set_name__
+            except AttributeError:
+                continue
+
+            set_name(self, field_name)
+
     @property
     def storage_name(self):
         return inflection.tableize(self.__name__)
