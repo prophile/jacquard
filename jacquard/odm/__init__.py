2d725fcb125271fd94acf1719808a8975b93b3e3
Alistair Lynn <arplynn@gmail.com>
2017-03-08 10:36:12 +0000

    Export the rest of the useful bits from the ODM

--- jacquard/odm/__init__.py
+++ jacquard/odm/__init__.py
@@ -1,9 +1,12 @@
 from .base import Model
-from .fields import TextField
-from .session import Session
+from .fields import TextField, JSONField, BaseField
+from .session import Session, transaction
 
 __all__ = (
     'Model',
     'TextField',
+    'JSONField',
+    'BaseField',
     'Session',
+    'transaction',
 )
