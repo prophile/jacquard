cbf60f54610d1361e72ed68859dbdda9f4d56696
Dan Palmer <dan@danpalmer.me>
2017-07-01 17:21:04 +0100

    Variable annotations are new in 3.6 so use comments for 3.5 support

--- jacquard/storage/cloned_redis.py
+++ jacquard/storage/cloned_redis.py
@@ -6,7 +6,7 @@ import pickle
 import logging
 import warnings
 import threading
-from typing import Any, Dict
+from typing import Any, Dict  # noqa: F401
 
 import redis
 
@@ -16,7 +16,7 @@ from .exceptions import Retry
 LOGGER = logging.getLogger('jacquard.storage.cloned_redis')
 
 
-_REDIS_POOL: Dict[str, Any] = {}
+_REDIS_POOL = {}  # type: Dict[str, Any]
 _REDIS_POOL_LOCK = threading.Lock()
 
 
