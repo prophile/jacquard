a70c16ce353f1a7280784f61c88c6d5600fada8c
Dan Palmer <dan@danpalmer.me>
2017-07-01 17:11:41 +0100

    Add initial type for Redis pool to get checker passing

--- jacquard/storage/cloned_redis.py
+++ jacquard/storage/cloned_redis.py
@@ -6,6 +6,7 @@ import pickle
 import logging
 import warnings
 import threading
+from typing import Dict, Any
 
 import redis
 
@@ -15,7 +16,7 @@ from .exceptions import Retry
 LOGGER = logging.getLogger('jacquard.storage.cloned_redis')
 
 
-_REDIS_POOL = {}
+_REDIS_POOL: Dict[str, Any] = {}
 _REDIS_POOL_LOCK = threading.Lock()
 
 
