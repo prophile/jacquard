f2fd224b5e3c8cb4a919e082c47c603d4469a564
Alistair Lynn <arplynn@gmail.com>
2017-08-29 19:02:53 +0100

    Use an explicit test here

--- jacquard/buckets/tests/test_bucket.py
+++ jacquard/buckets/tests/test_bucket.py
@@ -25,4 +25,6 @@ def test_at_least_three_buckets_per_percent():
 def test_can_get_empty_bucket_from_old_format():
     session = Session({'buckets/1': []})
     bucket = session.get(Bucket, 1)
-    assert not bucket.needs_constraints()
+    # Force bucket to a string in order to reify the fields. This validates
+    # that the fields are accessible.
+    str(bucket)
