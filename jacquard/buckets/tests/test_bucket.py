91bb9574ec760efd8aba2d9ae8fe67fe2e69d0a2
Alistair Lynn <arplynn@gmail.com>
2017-08-29 17:04:56 +0100

    Add a test for getting an empty bucket

--- jacquard/buckets/tests/test_bucket.py
+++ jacquard/buckets/tests/test_bucket.py
@@ -1,5 +1,7 @@
 import pytest
 
+from jacquard.odm import Session
+from jacquard.buckets import Bucket
 from jacquard.buckets.constants import NUM_BUCKETS
 
 
@@ -18,3 +20,9 @@ def test_divisible(divisor):
 
 def test_at_least_three_buckets_per_percent():
     assert NUM_BUCKETS / 100 >= 3
+
+
+def test_can_get_empty_bucket_from_old_format():
+    session = Session({'buckets/1': []})
+    bucket = session.get(Bucket, 1)
+    assert not bucket.needs_constraints()
