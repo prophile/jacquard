b47821b4fce6ab969fab3c7c5a1ef1a8fb58764c
Alistair Lynn <arplynn@gmail.com>
2017-10-19 09:25:56 +0100

    Cover this exception with a test

--- jacquard/storage/tests/test_dummy.py
+++ jacquard/storage/tests/test_dummy.py
@@ -1,3 +1,4 @@
+import pytest
 import unittest
 
 from jacquard.storage.dummy import DummyStore
@@ -7,3 +8,15 @@ from jacquard.storage.testing_utils import StorageGauntlet
 class DummyGauntletTest(StorageGauntlet, unittest.TestCase):
     def open_storage(self):
         return DummyStore('')
+
+    def test_transaction_raises_error_for_bad_commit(self):
+        store = self.open_storage()
+        transaction = store.transaction(read_only=True)
+        transaction_map = transaction.__enter__()
+
+        transaction_map['new_key'] = 'new_value'
+
+        with pytest.raises(RuntimeError):
+            transaction.__exit__(None, None, None)
+
+        assert 'new_key' not in store.data
